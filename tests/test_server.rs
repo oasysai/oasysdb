794aa128581ee8f9002564a62288aebe3fd9fc47
Edwin Kys <itsedwinkys@gmail.com>
2023-12-01 01:00:43 -0600

    feat: add test case for creating kvs

--- tests/test_server.rs
+++ tests/test_server.rs
@@ -1,24 +1,46 @@
 mod common;
 
 use common::*;
-use serde_json::Value;
 
 #[tokio::test]
 async fn test_get_root() {
     let runtime = run_server().await;
 
-    // Check the server status.
-    let url = format!("http://{}:{}/", HOST, PORT);
+    // Make a GET request to the root.
+    let url = format!("http://{}:{}", HOST, PORT);
     let res = reqwest::get(url).await.unwrap();
 
-    // Unwrap the response.
-    let code = res.status();
-    let body = res.text().await.unwrap();
-    let json: Value = serde_json::from_str(&body).unwrap();
-
     // Assert the response.
-    assert_eq!(code, 200);
-    assert_eq!(json.get("status").unwrap(), "ok");
+    assert_eq!(res.status(), 200);
+
+    stop_server(runtime).await;
+}
+
+#[tokio::test]
+async fn test_post_kvs() {
+    let runtime = run_server().await;
+
+    let url = format!("http://{}:{}/kvs", HOST, PORT);
+
+    // Create raw JSON string for the body.
+    let json = r#"{
+        "key": "test_key",
+        "value": {"embedding": [0.0], "data": {}}
+    }"#;
+
+    // Make a post request.
+    let client = reqwest::Client::new();
+    let res = client.post(&url).body(json).send().await;
+
+    // Get the response code.
+    let code = if res.is_ok() {
+        res.unwrap().status().as_u16()
+    } else {
+        500
+    };
+
+    // Assert the response code.
+    assert_eq!(code, 201);
 
     stop_server(runtime).await;
 }
