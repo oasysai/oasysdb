0d983440053ade395178cfb47a5622de5e1e64c8
Edwin Kys <itsedwinkys@gmail.com>
2024-01-15 20:26:59 -0600

    feat: add test cases for index

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,2 +1,54 @@
 mod test_database;
 mod test_index;
+
+use crate::index::*;
+use crate::vector::*;
+use rand::random;
+
+fn create_test_index<const N: usize>(
+    records: &[IndexRecord<usize, N>],
+) -> IndexGraph<usize, N> {
+    let config = IndexConfig::default();
+    IndexGraph::build(&config, &records)
+}
+
+fn gen_records<const N: usize>(len: usize) -> Vec<IndexRecord<usize, N>> {
+    let mut records = Vec::with_capacity(len);
+
+    for _ in 0..len {
+        let vector = gen_vector::<N>();
+        let data = random::<usize>();
+        records.push(IndexRecord { vector, data });
+    }
+
+    records
+}
+
+fn gen_vector<const N: usize>() -> Vector<N> {
+    let mut vec = [0.0; N];
+
+    for float in vec.iter_mut() {
+        *float = random::<f32>();
+    }
+
+    Vector(vec)
+}
+
+fn brute_force_search<const N: usize>(
+    records: &[IndexRecord<usize, N>],
+    query: &Vector<N>,
+    n: usize,
+) -> Vec<(f32, usize)> {
+    let mut nearest = Vec::with_capacity(records.len());
+
+    // Calculate the distance between the query and each record.
+    for record in records {
+        let distance = query.distance(&record.vector);
+        nearest.push((distance, record.data));
+    }
+
+    // Sort the nearest neighbors by distance.
+    nearest.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+    nearest.truncate(n);
+    nearest
+}
--- src/tests/test_index.rs
+++ src/tests/test_index.rs
@@ -1,1 +1,59 @@
+use super::*;
 
+#[test]
+fn test_index_build_large() {
+    let len = 10000;
+    let records = gen_records::<128>(len);
+    let index = create_test_index::<128>(&records);
+    assert_eq!(index.data.len(), len);
+}
+
+#[test]
+fn test_index_insert() {
+    let len = 100;
+    let records = gen_records::<128>(len);
+    let mut index = create_test_index::<128>(&records);
+
+    let vector = gen_vector::<128>();
+    let data = random::<usize>();
+
+    let id = VectorID(len as u32);
+    index.insert(&IndexRecord { vector, data });
+
+    assert_eq!(index.data.len(), len + 1);
+    assert_eq!(index.data[&id], data);
+}
+
+#[test]
+fn test_index_delete() {
+    let len = 100;
+    let records = gen_records::<128>(len);
+    let mut index = create_test_index::<128>(&records);
+
+    let id = VectorID(1);
+    index.delete(&id);
+
+    assert_eq!(index.data.len(), len - 1);
+    // Ensure the data is deleted.
+    assert_eq!(index.data.contains_key(&id), false);
+}
+
+#[test]
+fn test_index_search() {
+    let len = 100;
+    let records = gen_records::<128>(len);
+    let index = create_test_index::<128>(&records);
+    let query = gen_vector::<128>();
+
+    let result = index.search(&query, 5);
+    let truth = brute_force_search(&records, &query, 10);
+
+    // Collect the distances from the true nearest neighbors.
+    let truth_distances: Vec<f32> = truth.iter().map(|i| i.0).collect();
+
+    assert_eq!(result.len(), 5);
+
+    // The search is not always exact, so we check if
+    // the distance is within the true distances.
+    assert_eq!(truth_distances.contains(&result[0].distance), true);
+}
