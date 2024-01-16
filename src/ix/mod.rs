75dbb22f167af7686b15ab0d1de1e8d3784a8fa6
Edwin Kys <itsedwinkys@gmail.com>
2024-01-15 20:40:33 -0600

    chore: add credit to instant distance library

--- src/ix/mod.rs
+++ src/ix/mod.rs
@@ -17,3 +17,8 @@ use serde_big_array::BigArray;
 use std::cmp::*;
 use std::collections::{BinaryHeap, HashMap};
 use std::ops::{Deref, Index};
+
+// This code is inspired by the HNSW implementation in the
+// Instant Distance library and modified to fit the needs
+// of this project.
+// https://github.com/instant-labs/instant-distance
