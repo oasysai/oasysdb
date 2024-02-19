081db9089e3cb9f6da20e2d273156ffaf272363e
Edwin Kys <itsedwinkys@gmail.com>
2024-02-19 00:46:04 -0600

    feat: add initial setup of pyo3

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 #![doc(html_favicon_url = "https://i.postimg.cc/W3T230zk/favicon.png")]
 #![doc(html_logo_url = "https://i.postimg.cc/Vv0HPVwB/logo.png")]
 
+#[cfg(test)]
+mod tests;
+
 mod db;
 mod func;
 
@@ -10,5 +13,16 @@ pub use db::database;
 pub use func::collection;
 pub use func::vector;
 
-#[cfg(test)]
-mod tests;
+use pyo3::prelude::*;
+
+#[pymodule]
+fn oasysdb(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    collection_module(py, m)?;
+    Ok(())
+}
+
+fn collection_module(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    let module = PyModule::new(py, "collection")?;
+    m.add_submodule(module)?;
+    Ok(())
+}
