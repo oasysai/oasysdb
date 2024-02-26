6078517c2c9ab9bc6b71faf5c98a83f4404aeaf7
Edwin Kys <itsedwinkys@gmail.com>
2024-02-25 21:48:16 -0600

    feat: add script to measure memory

---
+++ examples/measure-memory.rs
@@ -0,0 +1,22 @@
+use jemalloc_ctl::stats::allocated;
+use jemallocator::Jemalloc;
+use oasysdb::collection::*;
+
+#[global_allocator]
+static ALLOC: Jemalloc = Jemalloc;
+
+fn main() {
+    // Modify as needed.
+    let len = 10000;
+    let dimension = 128;
+
+    // Build the vector collection.
+    let records = Record::many_random(dimension, len);
+    let config = Config::default();
+    Collection::build(&config, &records).unwrap();
+
+    // Measure the memory usage.
+    let memory = allocated::read().unwrap();
+    println!("For {} vector records of dimension {}", len, dimension);
+    println!("Memory usage: {} bytes", memory);
+}
