a690b758a0b5558fca0f9befe0cfab1b935ba266
Edwin Kys <itsedwinkys@gmail.com>
2024-02-29 22:51:25 -0600

    fix: implement debug trait for collection & config (#35)

--- src/func/collection.rs
+++ src/func/collection.rs
@@ -2,7 +2,7 @@ use super::*;
 
 /// The collection HNSW index configuration.
 #[pyclass(module = "oasysdb.collection")]
-#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub struct Config {
     /// Nodes to consider during construction.
     #[pyo3(get, set)]
