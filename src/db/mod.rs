585c189d2eaf67b8ad5d8bc6e7372ac96386fc6d
Edwin Kys <itsedwinkys@gmail.com>
2024-06-08 11:54:08 -0500

    fix: atomic collection file writes (#99)

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,11 +2,11 @@
 pub mod database;
 
 use crate::collection::*;
-use crate::func::err::Error;
+use crate::func::err::{Error, ErrorKind};
 use serde::{Deserialize, Serialize};
 use sled::Db;
 use std::collections::hash_map::DefaultHasher;
-use std::fs::{create_dir_all, remove_dir_all, remove_file, OpenOptions};
+use std::fs::{self, OpenOptions};
 use std::hash::{Hash, Hasher};
 use std::io::{BufReader, BufWriter};
 use std::path::Path;
