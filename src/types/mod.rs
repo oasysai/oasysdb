4d756e38e76812e51ba97e9f25ab9d3182189083
Edwin Kys <itsedwinkys@gmail.com>
2024-08-22 01:43:05 -0500

    refactor: remove cluster & record id types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,17 +1,12 @@
 // Initialize modules without publicizing them.
-mod cluster;
 mod error;
 mod metric;
-mod record;
 mod vector;
 
 // Re-export types from the modules.
-pub use cluster::*;
 pub use error::*;
 pub use metric::*;
-pub use record::*;
 pub use vector::*;
 
 // Import common dependencies below.
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
