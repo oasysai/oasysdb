f801b63843a6a49d62de9b4b89bdc1ae32371603
Edwin Kys <itsedwinkys@gmail.com>
2024-09-28 21:49:00 -0500

    feat: add record type

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,10 +1,13 @@
 // Initialize modules without publicizing them.
 mod metric;
+mod record;
 mod vector;
 
 // Re-export types from the modules.
 pub use metric::*;
+pub use record::*;
 pub use vector::*;
 
 // Import common dependencies below.
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
---
+++ src/types/record.rs
@@ -0,0 +1,32 @@
+use super::*;
+use uuid::Uuid;
+
+/// Record Identifier.
+///
+/// OasysDB should be able to deal with a lot of writes and deletes. Using UUID
+/// version 4 to allow us to generate a lot of IDs with very low probability
+/// of collision.
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Clone, Copy)]
+pub struct RecordID(Uuid);
+
+/// Metadata value.
+///
+/// OasysDB doesn't support nested objects in metadata for performance reasons.
+/// We only need to support primitive types for metadata.
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Value {
+    Text(String),
+    Number(f64),
+    Boolean(bool),
+}
+
+/// OasysDB vector record.
+///
+/// This is the main data structure for OasysDB. It contains the vector data
+/// and metadata of the record. Metadata is a key-value store that can be used
+/// to store additional information about the vector.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Record {
+    vector: Vector,
+    metadata: HashMap<String, Value>,
+}
